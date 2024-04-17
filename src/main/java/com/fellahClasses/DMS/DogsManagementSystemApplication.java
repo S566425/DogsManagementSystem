dd6a6791bf111a208cc2b697431cf5c9f92c65bc
Sri Vasavi Peravarapu <143015254+S566425@users.noreply.github.com>
2024-04-17 17:15:12 -0500

    push the DogsManagementSystem code into repository

---
+++ src/main/java/com/fellahClasses/DMS/DogsManagementSystemApplication.java
@@ -0,0 +1,16 @@
+package com.fellahClasses.DMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Sri Vasavi Peravarapu
+ */
+@SpringBootApplication
+public class DogsManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogsManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/fellahClasses/DMS/repository/TrainerRepository.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.fellahClasses.DMS.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.fellahClasses.DMS.Models.Trainer;
+
+/**
+ * @author Sri Vasavi Peravarapu
+ */
+public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
+
+}
